0ada36a5fef5b19c0d21ee61f1cc9dfbe9a99a29
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 01:19:44 +0200

    Add properties to MyBoardsContext.cs

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -9,6 +9,10 @@ namespace MyBoards.Entities
 {
     public class MyBoardsContext : DbContext
     {
-
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
