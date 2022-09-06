df95fda48810725a10c216ec568bc3f3e5bcedc4
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 00:53:07 +0200

    Add properties to Address.cs

--- MyBoards/Entities/Address.cs
+++ MyBoards/Entities/Address.cs
@@ -8,6 +8,9 @@ namespace MyBoards.Entities
 {
     public class Address
     {
-
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
     }
 }
