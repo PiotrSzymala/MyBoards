15efde4c0aeb12a21c25fce5f03389455dacb67a
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 00:49:58 +0200

    Add properties to Tag.cs

--- MyBoards/Entities/Tag.cs
+++ MyBoards/Entities/Tag.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyBoards.Entities
 {
     public class Tag
     {
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
