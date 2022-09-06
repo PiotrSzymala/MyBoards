b96c9f638de1a178d7cf3c1e2cd87f41d932f569
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 01:17:30 +0200

    Add properties to User.cs

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -8,6 +8,8 @@ namespace MyBoards.Entities
 {
     public class User
     {
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
