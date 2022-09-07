8c8a31ac41d72ace2cd4b8fc8850a413e13162be
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 02:19:59 +0200

    Set attributes by overriding OnModelCreating method

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.EntityFrameworkCore;
 
 namespace MyBoards.Entities
@@ -19,5 +20,24 @@ namespace MyBoards.Entities
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Address> Addresses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.State)
+                .IsRequired();
+
+            modelBuilder.Entity<WorkItem>()
+                .Property(x => x.Area)
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.RemainWork).HasPrecision(14, 2);
+            });
+        }
     }
 }
