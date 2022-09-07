a08d562f2f45038412f8ac7db8ca7ebbd3d05547
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 02:04:08 +0200

    Add attributes to properties

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyBoards.Entities
 {
@@ -10,16 +13,22 @@ namespace MyBoards.Entities
     {
         public int Id { get; set; }
         public string State { get; set; }
+        [Column(TypeName = "varchar(200)")]
         public string Area { get; set; }
+        [Column("Iteration_Path")]
         public string IterationPath { get; set; }
         public int Priority { get; set; }
         // Epic
         public DateTime? StartDate { get; set; }
+        [Precision(3)]
         public DateTime? EndDate { get; set; }
         // Issue
+        [Column(TypeName = "decimal(5,2)")]
         public decimal Efford { get; set; }
         // Task
+        [MaxLength(200)]
         public string Activity { get; set; }
+        [Precision(14,2)]
         public decimal RemainWork { get; set; }
 
 
