025421b85ee2a66f13e9332250677ee9fba3264d
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 15:12:12 +0200

    Add properties to WorkItemTag.cs

--- MyBoards/Entities/WorkItemTag.cs
+++ MyBoards/Entities/WorkItemTag.cs
@@ -8,6 +8,9 @@ namespace MyBoards.Entities
 {
     public class WorkItemTag
     {
-      
+        public WorkItem WorkItem { get; set; }
+        public int WorkItemId { get; set; }
+        public Tag Tag { get; set; }
+        public int TagId { get; set; }
     }
 }
