d121cc704e194393266550c7c5663a17caaf3e23
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-07 16:07:42 +0200

    Create dependencies between WorkItem and WorkItemState

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -12,8 +12,9 @@ namespace MyBoards.Entities
     public class WorkItem
     {
         public int Id { get; set; }
-        public string State { get; set; }
         public string Area { get; set; }
+        public WorkItemState State { get; set; }
+        public int StateId { get; set; }
         public string IterationPath { get; set; }
         public int Priority { get; set; }
         // Epic
--- MyBoards/Entities/WorkItemState.cs
+++ MyBoards/Entities/WorkItemState.cs
@@ -8,6 +8,7 @@ namespace MyBoards.Entities
 {
     public class WorkItemState
     {
-
+        public int Id { get; set; }
+        public string Value { get; set; }
     }
 }
