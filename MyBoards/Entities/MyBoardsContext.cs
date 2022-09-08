93f6e0a2cf086cb2fdcce4bfe685c8d2989bd43e
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-08 22:54:28 +0200

    User and Comment ention refactor

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -88,6 +88,11 @@ namespace MyBoards.Entities
             {
                 eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
                 eb.Property(x => x.UpdatedDate).ValueGeneratedOnUpdate();
+                
+                eb.HasOne(c => c.Author)
+                    .WithMany(a => a.Comments)
+                    .HasForeignKey(c => c.AuthorId)
+                    .OnDelete(DeleteBehavior.NoAction);
             });
 
             modelBuilder.Entity<User>()
