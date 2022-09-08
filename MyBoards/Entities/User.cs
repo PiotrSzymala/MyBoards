910a38be1bbde438e1a74bac945402def65071b3
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-08 16:18:57 +0200

    Create custom migration (refactor fields Firtst Name and Last name to one field - Full Name)

--- MyBoards/Entities/User.cs
+++ MyBoards/Entities/User.cs
@@ -9,8 +9,7 @@ namespace MyBoards.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public Address Address { get; set; }
         public List<WorkItem> WorkItems { get; set; } = new List<WorkItem>();
