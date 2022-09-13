d0ec4b63278dbed6cf8b29074bdb4aee6bb57864
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-09-13 17:50:42 +0200

    Add enviroment to edit data

--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -69,4 +69,16 @@ app.MapGet("data", async (MyBoardsContext db) =>
     return statesCount;
 });
 
+app.MapPost("update", async (MyBoardsContext db) =>
+{
+    Epic epic = await db.Epics.FirstAsync(epic => epic.Id == 1);
+
+    var rejectedState = await db.WorkItemStates.FirstAsync(a => a.Value == "Rejected");
+
+    epic.State = rejectedState;
+
+    await db.SaveChangesAsync();
+
+    return epic;
+});
 app.Run();
